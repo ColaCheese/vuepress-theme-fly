fb9f334be4a911d01bd90ce4f036e63e16011ebc
LV-YY <38833097+Lvue-YY@users.noreply.github.com>
2020-07-23 22:29:52 +0800

    change

--- docs/.vuepress/components/MyHome.vue
+++ docs/.vuepress/components/MyHome.vue
@@ -1,20 +1,25 @@
 <template>
 <div>
-    <div class="wrapper"></div>
+    <div class="wrapper">
+        <div class="head">Do Creative Work</div>
+        <v-btn class="head-btn" outlined color="#ffffff" large>
+            START&nbsp<v-icon right>mdi-arrow-right</v-icon>
+        </v-btn>
+    </div>
     <v-footer class="my-footer" absolute color="#FFFFFF">
         <span style="margin-top: 12px; font-size: 14px;">
             &copy;
             {{ (new Date()).getFullYear() }}
             <a href="https://github.com/Love-YY" target="_blank">LOVEYY </a>
-            BLOG 
+            BLOG
             <v-icon color="#FF5252" small>mdi-heart</v-icon>
             RECORD
         </span>
         <span style="margin-top: 14px; font-size: 15px;">
-          Power By&nbsp&nbsp
-          <a href="https://github.com/vuejs/vuepress" target="_blank"> vuepress </a>|
-          <a href="https://github.com/vuepress-reco" target="_blank"> vuepress-reco </a>|
-          <a href="https://github.com/vuetifyjs/vuetify" target="_blank"> vuetify</a>
+            Power By&nbsp&nbsp
+            <a href="https://github.com/vuejs/vuepress" target="_blank"> vuepress </a>|
+            <a href="https://github.com/vuepress-reco" target="_blank"> vuepress-reco </a>|
+            <a href="https://github.com/vuetifyjs/vuetify" target="_blank"> vuetify</a>
         </span>
     </v-footer>
 
@@ -53,6 +58,10 @@ export default {};
     background-position: center bottom;
     background-repeat: no-repeat;
     background-attachment: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
 }
 
 .my-footer {
@@ -60,5 +69,37 @@ export default {};
     display: flex;
     flex-direction: column;
     justify-content: center;
+}
+
+.head {
+    color: #ffffff;
+    font-size: 80px;
+    font-weight: 100;
+    animation: show 3s ease-in-out;
+}
+
+.head-btn {
+    margin-top: 70px;
+    animation: show 4s ease-in-out;
+}
+
+@keyframes show {
+    from {
+        transform: translateY(-50px);
+        opacity: 0.1;
+    }
+
+    to {
+        transform: translateY(0px);
+        opacity: 1;
+    }
+}
+
+@media (max-width: 600px) {
+    .head {
+        color: #ffffff;
+        font-size: 40px;
+        font-weight: 100;
+    }
 }
 </style>
